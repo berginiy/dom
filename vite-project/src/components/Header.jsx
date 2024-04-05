e2a205d42305efb59d6c30c317c948b0d8889b64
berginiy <pirozokaltynbek@gmail.com>
2024-04-05 10:14:13 +0600

    uio

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import {UserContext} from '../App.jsx'
 
 export default function Header() {
     const [user, setUser] = useContext(UserContext)
--- vite-project/src/components/SignIn.jsx
+++ vite-project/src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react'
 import axios from 'axios'
-import { UserContext } from '@/App.jsx'
+import { UserContext } from '../App.jsx'
 import { useNavigate } from "react-router-dom"
 
 export default function Register() {
